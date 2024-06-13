c5e698b66edb7b1d4cf2b1e3cf46648dc319475f
Suren <alexkalinin19756@mail.ru>
2024-06-13 13:37:23 +0300

    fix

--- tasks/seq/salaev_v_components_marking/func_tests/main.cpp
+++ tasks/seq/salaev_v_components_marking/func_tests/main.cpp
@@ -77,14 +77,15 @@ TEST(salaev_v_components_marking_seq_functional, correct_run) {
   ASSERT_EQ(testTaskSequential.run(), true);
 }
 
-TEST(salaev_v_components_marking_seq_functional, correct_post_processing) {
+TEST(salaev_v_components_marking_seq_functional, correct_rectangle) {
   // Create data
   uint32_t height = 5;
-  uint32_t width = 5;
+  uint32_t width = 6;
   std::vector<uint32_t> dimensions = {height, width};
-  std::vector<uint8_t> input = {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0};
+  std::vector<uint8_t> input = {0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1};
   std::vector<uint32_t> output(height * width, 0);
-  std::vector<uint32_t> expected = {0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9, 0, 10, 0, 11, 0, 12, 0};
+  std::vector<uint32_t> expected = {1, 0, 2, 0, 3, 0, 0, 4, 0, 5, 0, 6, 7, 0, 8, 0, 9, 0, 0, 10, 0, 11, 0, 12, 13, 0, 14, 0, 15, 0};
+
 
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
@@ -100,7 +101,8 @@ TEST(salaev_v_components_marking_seq_functional, correct_post_processing) {
   testTaskSequential.validation();
   testTaskSequential.pre_processing();
   testTaskSequential.run();
-  ASSERT_EQ(testTaskSequential.post_processing(), true);
+  testTaskSequential.post_processing();
+  ASSERT_EQ(output, expected);
 }
 
 TEST(salaev_v_components_marking_seq_functional, test_functional) {
